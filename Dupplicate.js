2070342c14928d3b81e3648fca56fe3dcbd95862
baliram123giri <gir71401@gmail.com>
2022-02-15 17:47:42 +0530

    class2

---
+++ Dupplicate.js
@@ -0,0 +1,10 @@
+const fruits = ["Bannana", "Apple", "Graps", "Apple"]
+
+//  const uniqe = [...new Set(fruits)]
+//  const uniqe = fruits.filter((value, index)=>{
+
+//      return fruits.indexOf(value)===index
+//  })
+const uniqe = [...new Set(fruits)]
+ console.log(uniqe)
+
